a76f0bcedef8c5497a0edef92f9536585e4b3f6d
Ahmed M. ElSAyed <compiler@aast.edu>
2024-11-15 15:33:07 +0200

    1-Fix time calculation, 2-Add method to calc mm using blocked mm

--- src/edu/aast/cndc/javaoop/DenseMatrix.java
+++ src/edu/aast/cndc/javaoop/DenseMatrix.java
@@ -1,7 +1,8 @@
 package edu.aast.cndc.javaoop;
 
-import java.time.LocalTime;
-import java.time.Duration;
+
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 public class DenseMatrix {
@@ -10,8 +11,8 @@ public class DenseMatrix {
 	final double rangeMin,rangeMax;
 	
 	//Time taken after certain operation. Calculated inside the operation method
-	private Duration t2_1;
-	private double ElapsedTime=0;
+	
+	private double ElapsedTime;
 	
 	//Default constructor
 	DenseMatrix() {
@@ -25,6 +26,7 @@ public class DenseMatrix {
 		matrix = new double[rows][cols];
 		this.rangeMin = 0;
 		this.rangeMax = 1000;
+		this.ElapsedTime = 0;
 		InitMatrix(); 
 	}
 	
@@ -70,23 +72,42 @@ public class DenseMatrix {
 	//Multiply a matrix in sequential using normal matrix mult
 	public DenseMatrix MultiplySeq(DenseMatrix b) {
 		int i,j,k;
+		Date startTime,endTime;
 		DenseMatrix outm;
 		outm=new DenseMatrix(this.rows,this.cols);
 		outm.InitMatrix(0);
 		
-		LocalTime t1 = LocalTime.now();
+		startTime=Calendar.getInstance().getTime();
 		for(i=0;i<this.rows;i++) 
 			for(j=0;j<this.cols;j++)
 				for(k=0;k<this.rows;k++)
 					outm.matrix[i][j] += this.matrix[i][k] * b.matrix[k][j];	
-		LocalTime t2 = LocalTime.now();
-		this.t2_1 = Duration.between(t1, t2);
-/*		if ( t2_1 == null )
-			System.out.println("Null time duration");
-		else*/
-			this.ElapsedTime=this.t2_1.getSeconds();
+		endTime=Calendar.getInstance().getTime();
+		outm.ElapsedTime=( endTime.getTime() - startTime.getTime() ) / 1000 % 60;
 		return outm;
 	}
+	
+	//Multiply a matrix in sequential using blocked matrix mult
+	public DenseMatrix MultiplySeqBlocked(DenseMatrix b,int bs) {
+		int i,j,k;
+		int ii,jj,kk;
+		Date startTime,endTime;
+		DenseMatrix outm;
+		outm=new DenseMatrix(this.rows,this.cols);
+		outm.InitMatrix(0);
+		startTime=Calendar.getInstance().getTime();
+		for(ii=0;ii<this.rows;ii+=bs)
+			for(jj=0;jj<this.cols;jj+=bs)
+				for(kk=0;kk<this.rows;kk+=bs)
+					for(i=ii;i<(ii+bs);i++)
+						for(j=jj;j<(jj+bs);j++)
+							for(k=kk;k<(kk+bs);k++)
+								outm.matrix[i][j]+=this.matrix[i][k]*b.matrix[k][j];
+		endTime=Calendar.getInstance().getTime();
+		outm.ElapsedTime=( endTime.getTime() - startTime.getTime() ) / 1000 % 60;
+		return outm;
+	}
+
 	
 	public double getElapsedTime() {
 		return this.ElapsedTime;
--- src/edu/aast/cndc/javaoop/JavaOOP.java
+++ src/edu/aast/cndc/javaoop/JavaOOP.java
@@ -7,9 +7,9 @@ public class JavaOOP {
 		// TODO Auto-generated method stub
 		DenseMatrix a,b,c;
 		System.out.println("Start .. ");
-		a=new DenseMatrix(1500);
-		b=new DenseMatrix(1500);
-		c=new DenseMatrix(1500);
+		a=new DenseMatrix(1600);
+		b=new DenseMatrix(1600);
+		c=new DenseMatrix(1600);
 		a.InitMatrix(1);
 		b.InitMatrix(2);
 /*
@@ -23,7 +23,12 @@ public class JavaOOP {
 		System.out.println("C After : ");
 		c.PrintMatrix();
 */
-		System.out.println("Tooks times: " + c.getElapsedTime() + " seconds");
+		System.out.println("Tooks times for normal seq: " + c.getElapsedTime() + " seconds");
+		
+		
+		c=a.MultiplySeqBlocked(b,8);
+		System.out.println("Tooks times for blocked seq: " + c.getElapsedTime() + " seconds");
+		
 		
 
 
